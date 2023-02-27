0a2bf99a932fb5a23e4f63bc072656f4a39df0b4
Winston Wolfe <pabz1038@gmail.com>
2023-02-27 14:13:18 -0700

    finished exercise 6.

---
+++ SmallProblems/Easy1/ex6.js
@@ -0,0 +1,50 @@
+const rl = require('readline-sync');
+
+function prompt(msg) {
+  console.log(msg);
+}
+
+function computeSum(givenNum) {
+  let total = 0;
+
+  for (let num = 1; num <= givenNum; num += 1) {
+    total += num;
+  }
+  return total;
+}
+
+function returnProduct(givenNum) {
+  let total = 1;
+
+  for (let num = 1; num <= givenNum; num += 1) {
+    total *= num;
+  }
+  return total;
+}
+
+function invalidNum(num) {
+  return num === '' || Number(num) <= 0 || Number.isNaN(Number(num));
+}
+
+prompt('please enter a number greater than 0: ');
+let number = parseInt(rl.question());
+
+while (invalidNum(number)) {
+  prompt('Please enter valid number.');
+  number = parseInt(rl.question());
+}
+
+prompt("Enter 's' for sum, or 'p' for product. ");
+let answer = rl.question().toLowerCase();
+
+while (!['s', 'p'].includes(answer)) {
+  prompt("Please enter 's' or 'p'.")
+  answer = rl.question().toLowerCase();
+}
+if (answer === 's') {
+  let sum = computeSum(number)
+  console.log(sum);
+} else if (answer === 'p') {
+  let product = returnProduct(number);
+  prompt(product);
+}
