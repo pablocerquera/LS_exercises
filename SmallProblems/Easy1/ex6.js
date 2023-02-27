c77f06f203c9fc74b76368e4238545d3e29635bf
Winston Wolfe <pabz1038@gmail.com>
2023-02-27 14:21:03 -0700

    updated exercise 6.

--- SmallProblems/Easy1/ex6.js
+++ SmallProblems/Easy1/ex6.js
@@ -27,22 +27,22 @@ function invalidNum(num) {
 }
 
 prompt('please enter a number greater than 0: ');
-let number = parseInt(rl.question());
+let number = Number(rl.question());
 
 while (invalidNum(number)) {
   prompt('Please enter valid number.');
-  number = parseInt(rl.question());
+  number = Number(rl.question());
 }
 
 prompt("Enter 's' for sum, or 'p' for product. ");
 let answer = rl.question().toLowerCase();
 
 while (!['s', 'p'].includes(answer)) {
-  prompt("Please enter 's' or 'p'.")
+  prompt("Please enter 's' or 'p'.");
   answer = rl.question().toLowerCase();
 }
 if (answer === 's') {
-  let sum = computeSum(number)
+  let sum = computeSum(number);
   console.log(sum);
 } else if (answer === 'p') {
   let product = returnProduct(number);
